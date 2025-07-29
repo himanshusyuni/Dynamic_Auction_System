946058bc96f25be197b99f72cadb73be245fea35
Himanshu Syuni <himanshusyuni@Himanshus-MacBook-Air.local>
2025-07-29 20:51:01 +0530

    i

--- frontend/src/Pages/Ship_Del.jsx
+++ frontend/src/Pages/Ship_Del.jsx
@@ -8,6 +8,12 @@ const Shipping = () => {
         <strong>Last updated on Feb 6 2025</strong>
     </p>
       <p className="text-center">Shipping and Delivery is not applicable for business.</p>
+      <div className="mt-6 space-y-4">
+                <p><strong>Merchant Legal Entity Name:</strong> Himanshu</p>
+                <p><strong>Registered Address:</strong> 1057/A, Mehrauli , New Delhi</p>
+                <p><strong>Telephone No:</strong> <a href="tel:8595602610" className="text-blue-600 hover:underline">8595602610</a></p>
+                <p><strong>Email:</strong> <a href="mailto:himanshusyuni2601@gmail.com" className="text-blue-600 hover:underline">himanshusyuni2601@gmail.com</a></p>
+            </div>
     </div>
   );
 };
